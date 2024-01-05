8c06c3536bdbb96c821b56ab812c8de824e678f6
Oddem-Neha <20r01a05n4@gmail.com>
2024-01-05 15:23:15 +0530

    first commit

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import { signOut } from 'firebase/auth'
+import {auth} from '../firebase'
+import { AuthContext } from '../context/AuthContext'
+import { useContext } from 'react';
 const Navbar = () => {
+  const {currentUser}=useContext(AuthContext)
   return (
     <div className='navbar'>
         <span className='logo'>Neha</span>
         <div className='user'>
-            <img src='https://images.pexels.com/photos/18898429/pexels-photo-18898429/free-photo-of-close-up-of-a-wet-spiderweb-and-mountain-ash-berries.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load' alt=''/>
-            <span>Neha</span>
-            <button>Log out</button>
+            <img src= {currentUser.photoURL} alt=""/>
+            <span>{currentUser.displayName}</span>
+            <button onClick={()=>signOut(auth)}>Log out</button>
         </div>
     </div>
   )
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './context/AuthContext';
+import { ChatContextProvider } from './context/ChatContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+  <AuthContextProvider>
+<ChatContextProvider>
   <React.StrictMode>
     <App/>
   </React.StrictMode>
+  </ChatContextProvider>
+  </AuthContextProvider>
 );
 
 reportWebVitals();
